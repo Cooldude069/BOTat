d9078f8d6f5214a1afc8b338bf9a450d73a037c5
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-08-30 18:08:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def clear(ctx, amount=5):
 	await ctx.channel.purge(limit=amount + 1)
 	print(f'{amount} messages deleted')
 	await ctx.send(f"`{amount}` messages deleted")
-	time.sleep(3)
+	time.sleep(2)
 	await ctx.channel.purge(limit = 1)
 
 @client.command(aliases=['ssd', 'SSD', 'Ssd', 'Dilip', 'DILIP'])
