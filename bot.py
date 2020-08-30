a6069f69f08ac52d5753592f15bf277f3dd6a183
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-08-30 10:36:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from discord.ext import commands, tasks
 import random
 from itertools import cycle
 import youtube_dl
+import time
 import json
 import asyncio
 import shutil
@@ -11,18 +12,37 @@ from discord.utils import get
 
 client = commands.Bot(command_prefix="")
 status = cycle(['Fortnite on Android', 'Fortnite on Iphone'])
+global g
+g = 0
 
 @client.event
 async def on_ready():
 	change_status.start()
 	print("Bot is ready.")
 
-@client.command(aliases=['hi' , 'Hi' , 'Hola' , 'Sup'])
+@client.command(aliases=['hi' , 'Hi' , 'Hola' , 'Sup', 'sup', 'hola', 'Hello'])
 async def hello(ctx):
 	await ctx.send("Hi there!")
 
+@client.command(aliases=['AJO','Ajo'])
+async def ajo(ctx):
+	await ctx.send("BENJENE!!")
+
+@client.command(aliases=["nice", "Noice", "Nice"])
+async def noice(ctx):
+	await ctx.send("IKR!!")
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to The server!')
+
+@client.command(aliases=['Pop', 'POP'])
+async def pop(ctx):
+	await ctx.send("||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||\n||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||||pop||")
+     
 @client.command()
-async def unban(ctx, *, mamber):
+async def unban(ctx, *, member):
 	banned_users = await ctx.guild.bans()
 	member_name, member_discriminator = member.split('#')
 
@@ -38,13 +58,94 @@ async def unban(ctx, *, mamber):
 async def ping(ctx):
 	await ctx.send(f'Ping: {round(client.latency * 1000)} ms')
 
-@client.command()
+@client.command(aliases=['Oof', 'OOF'])
 async def oof(ctx):
 	await ctx.send("OOF!")
 
+@client.command()
+async def timer(ctx, *, Tm):
+	if Tm.endswith('r'):
+		tmr, un = Tm.split('h')
+		tm = float(tmr) * 3600
+		await ctx.send(f"Timer set for `{tmr}` hours")
+	elif Tm.endswith('s'):
+		tmr, un = Tm.split('s')
+		tm = float(tmr)
+		await ctx.send(f"Timer set for `{tmr}` seconds")
+	elif Tm.endswith('m'):
+		tmr, un = Tm.split('m')
+		await ctx.send(f"Timer set for `{tmr}` minutes")
+		tm = float(tmr) * 60
+	if tm > 3.0:
+		time.sleep(tm - 3)
+		await ctx.send("3")
+		time.sleep(1)
+		await ctx.send("2")
+		time.sleep(1)
+		await ctx.send("1")
+		time.sleep(1)
+		await ctx.send(":alarm_clock:Time Up:alarm_clock:")
+	else:
+		time.sleep(tm)
+		await ctx.send(":alarm_clock:Time Up:alarm_clock:")
+		return
+
+
 @client.command(aliases=['clean'])
 async def clear(ctx, amount=5):
-	await ctx.channel.purge(limit=amount)
+	await ctx.channel.purge(limit=amount + 1)
+	print(f'{amount} messages deleted')
+	await ctx.send(f"`{amount}` messages deleted")
+	time.sleep(3)
+	await ctx.channel.purge(limit = 1)
+
+@client.command(aliases=['ssd', 'SSD', 'Ssd', 'Dilip', 'DILIP'])
+async def dilip(ctx):
+	await ctx.send("Fodo!!")
+
+@client.command()
+async def popgame(ctx, length=5):
+	len = int(length)
+	row = random.randint(0, len - 1)
+	r = 0
+	while r < len:
+		if r == row:
+			await ctx.send("||POP||")
+		else:
+			await ctx.send("||pop ||")
+		r = r + 1
+
+@client.command()
+async def guess(ctx):
+	global g
+	g = 4	
+	global no
+	no = random.randint(0, 10)
+	await ctx.send("```Guess The number game created. Enter your number using the command, gnumber (number). You get three chances[Note: The number should lie between 0 and 10]```")
+
+@client.command()
+async def gnumber(ctx, gnum):
+	global g
+	if g > 2:
+		if int(gnum) == no:
+			await ctx.send(":partying_face: Congratulations!, You won :partying_face:")
+			g = 1
+		else:
+			await ctx.send(f"Better Luck next time.")
+			g = g - 1
+	elif g == 2:
+		if int(gnum) != no:
+			await ctx.send(f'Better luck next time. The number was {no}')
+			g = g - 1
+		else:
+			await ctx.send(":partying_face: Congratulations!, You won :partying_face:")
+			g = 1
+	elif g == 1:
+		await ctx.send("```You have used all of your chances```")
+		g = 0
+	else:
+		await ctx.send("```Guess the number game not created```")
+
 
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
@@ -56,12 +157,16 @@ async def ban(ctx, member : discord.Member, *, reason=None):
 	await member.ban(reason=reason)
 	await ctx.send(f'Banned {member.mention}')
 
+@client.command(aliases=['F'])
+async def f(ctx):
+	await ctx.send(f'You have paid your respects')
+
 @tasks.loop(minutes=15)
 async def change_status():
 	await client.change_presence(activity=discord.Game(next(status)))
 
 
-@client.command()
+@client.command(aliases=['8ball', 'Qna'])
 async def qna(ctx, *, question):
 	responses = ["It is certain.",
                  "As I see it, yes.",
@@ -87,12 +192,10 @@ async def qna(ctx, *, question):
 	await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
 
 
-
-@client.command()
+@client.command(aliases=['Binod', 'BINOD'])
 async def binod(ctx):
 	await ctx.send("BINOD!!")
 
-player = {}
 
 @client.command(pass_context=True)
 async def join(ctx):
@@ -129,8 +232,8 @@ async def play(ctx, url: str):
 				print("No more queued song(s)\n")
 				queues.clear()
 				return
-			main_location = os.path.dirname(os.path.realpath(__file__))
-			sing_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first__file)
+			main_location = os.path.dirname(os.path.realpath(_file_))
+			sing_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
 			if length != 0:
 				print("Song done. playing nest queued\n")
 				print(f'Songs still in queue: {still_q}')
@@ -143,8 +246,8 @@ async def play(ctx, url: str):
 						os.rename(file, 'song.mp3')
 
 				voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
-				voice.source = discord.PCMvolumrTransformer(voice.soure)
-				voice.source.volume = 0.07
+				voice.source = discord.PCMvolumrTransformer(voice.source)
+				voice.source.volume = 10.00
 
 			else:
 				queues.clear()
@@ -201,8 +304,7 @@ async def play(ctx, url: str):
 
 	voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
 	voice.source = discord.PCMVolumeTransformer(voice.source)
-	voice.source.volume = 0.07
-
+	voice.source.volume = 10.00
 	nname = name.rsplit(".", 2)
 	await ctx.send(f'Playing: {nname[0]}')
 	print("Playing\n")
