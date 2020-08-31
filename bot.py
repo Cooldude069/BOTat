9444d4df53ba34036312e2abe14474a1da7963e9
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-08-31 15:41:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,6 +19,20 @@ g = 0
 async def on_ready():
 	change_status.start()
 	print("Bot is ready.")
+	return 
+async def runtime_background_task():
+	colours = [0xFF0000, 0x00FF00, 0x0000FF0]
+	i = 0
+	
+	server = client.get_server("723435494578323476")
+	role = discord.utils.get(server.roles, name="Moderator")
+	
+	while not bot.is_closed:
+    	i = (i + 1) % 3
+    	await client.edit_role(server=server, role=role, colour=discord.Colour(colours[i]))
+	await asyncio.sleep(5)
+
+client.loop.create_task(runtime_background_task())
 	
 @client.command(pass_context=True)
 async def say(ctx, channel:discord.TextChannel , *, message):
