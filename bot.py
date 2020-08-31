0803e631b5094094cd4e349077495fac25c68b54
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-08-31 08:51:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,10 @@ async def hello(ctx):
 @client.command(aliases=['AJO','Ajo'])
 async def ajo(ctx):
 	await ctx.send("BENJENE!!")
+	
+@client.command()
+async def pin(*, reason=None):
+	await pin()
 
 @client.command(aliases=["nice", "Noice", "Nice"])
 async def noice(ctx):
