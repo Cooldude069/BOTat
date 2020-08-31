d4ab9c3ececc77e271ceec5963d401a2d6e146f7
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-08-31 13:50:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,10 +22,7 @@ async def on_ready():
 
 @client.command(aliases=['HELP', 'Help'])
 async def help(ctx):
-	member = ctx.message.author
-	await member.create_dm()
-	await ctx.send("Let me help you via DM")
-    	await member.dm_channel.send("`i have no prefix \n addrole/removerole {user} {role name}-> adds of removes a role from the mentioned user \n mute/unmute {user}-> mutes/unmutes a user \n kick {user} {reason}-> kick a user from the server \n ban/unban {user} {reason}-> Bans/unbans a user from a server \n clear {number}-> deletes the number of messages \n dmsend {user} {message}-> send a dm message to the user \n ping-> show the bot's ping \n pop -> make a bubble wrap \n timer {amount} {unit}-> sets a timer, the units can be s, m or hr`")
+    	await ctx.send("`i have no prefix \n addrole/removerole {user} {role name}-> adds of removes a role from the mentioned user \n mute/unmute {user}-> mutes/unmutes a user \n kick {user} {reason}-> kick a user from the server \n ban/unban {user} {reason}-> Bans/unbans a user from a server \n clear {number}-> deletes the number of messages \n dmsend {user} {message}-> send a dm message to the user \n ping-> show the bot's ping \n pop -> make a bubble wrap \n timer {amount} {unit}-> sets a timer, the units can be s, m or hr`")
 
 @client.command(aliases=['hi' , 'Hi' , 'Hola' , 'Sup', 'sup', 'hola', 'Hello'])
 async def hello(ctx):
