bc295942c84b617f7b505d613ca655b23268c98a
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-03 20:06:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -336,7 +336,7 @@ async def on_message(message):
 async def on_raw_reaction_add(reaction):
 	if reaction.channel_id == 750631580837609543:
 		if reaction.user_id == 727539383405772901:
-			if reaction.emoji == client.get_emoji(751072153813647431):
+			if reaction.emoji.id == 751072153813647431:
 				await reaction.message.pin(reason = None)
 				print("Message pinned")
 				return
