fd515d20d8c2da076906bae83925e7e7d3352709
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-03 15:29:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,30 +69,30 @@ async def handjob(ctx):
 
 @client.command(aliases = ["lb", "Leaderboard", "LEADERBOARD", "LB"])
 async def leaderboard(ctx,x = 1):
-    users = await get_bank_data()
-    leader_board = {}
-    total = []
-    for user in users:
-        name = int(user)
-        total_amount = users[user]["wallet"]
-        leader_board[total_amount] = name
-        total.append(total_amount)
+	users = await get_bank_data()
+	leader_board = {}
+	total = []
+	for user in users:
+		name = int(user)
+		total_amount = users[user]["wallet"]
+		leader_board[total_amount] = name
+		total.append(total_amount)
 
-    total = sorted(total,reverse=True)    
+	total = sorted(total,reverse=True)    
 
-    em = discord.Embed(title = f"Top memers" , description = "This is decided on the basis of number of memes by the user",color = discord.Color(0xfa43ee))
-    index = 1
-    for amt in total:
-        id_ = leader_board[amt]
-        member = client.get_user(id_)
-        name = member.name
-        em.add_field(name = f"{index}. {name}" , value = f"{amt}",  inline = False)
-        if index == x:
-            break
-	else:
+	em = discord.Embed(title = f"Top memers" , description = "This is decided on the basis of number of memes by the user",color = discord.Color(0xfa43ee))
+	index = 1
+	for amt in total:
+		id_ = leader_board[amt]
+		member = client.get_user(id_)
+		name = member.name
+		em.add_field(name = f"{index}. {name}" , value = f"{amt}",  inline = False)
+		if index == x:
+		break
+		else:
 		index += 1
 
-    await ctx.send(embed = em)
+	await ctx.send(embed = em)
 		
 
 async def open_account(user):
