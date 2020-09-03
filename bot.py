4040e1733df9c9855069b7ab539e9803d55d480c
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-03 19:09:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -335,7 +335,7 @@ async def on_message(message):
 @client.event
 async def on_reaction_add(reaction , user):
 	channel = reaction.message.channel
-	emj = discord.Emoji(name = 'r_fav' , id = '751072153813647431')
+	emj = discord.Emoji(<:r_fav:751072153813647431>)
 	if channel.name == '🤣meme-competiton' and user.display_name == "Samarth":
 		if reaction.emoji == emj:
 			await client.pin(reaction.message)
