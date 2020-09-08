5554ce1909d5b790711a3340b9b075229bffa5af
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-08 13:49:42 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -436,13 +436,18 @@ async def lock(ctx , lt = 0):
 	if ctx.message.author.guild_permissions.manage_channels:
 		await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
 		if lt > 0:
+			await ctx.send(f"Locked {ctx.message.channel.name} for `{lt}`s")
 			await asyncio.sleep(lt)
 			await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+			await ctx.send("Lockdown Ended")
+		else:
+			await ctx.send(f"Locked {ctx.message.channel.name} Indefinitely")
 		
 @client.command(aliases = ['UNLOCK' , 'Unlock'])
 async def unlock(ctx):
 	if ctx.message.author.guild_permissions.manage_channels:
 		await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+		await ctx.send(f"Unlocked {ctx.message.channel.name}")
 	
 @client.event
 async def on_message(message):
