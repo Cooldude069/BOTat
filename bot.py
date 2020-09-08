738134b060494127e39914f899db6fb2707464a2
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-08 15:45:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -430,40 +430,7 @@ async def slowend(ctx):
 	if ctx.message.author.guild_permissions.manage_channels:
 		await ctx.message.channel.edit(slowmode_delay = 0)
 		await ctx.send("Slowmode removed")
-		
-@client.command(aliases = ['Lockdown' , 'LOCKDOWN' , 'LOCK' , 'Lock' , 'lockdown'])
-async def lock(ctx , lt = 0):
-	if ctx.message.author.guild_permissions.manage_channels:
-		if not ctx.guild.default_role.read_messages :
-			await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-			await ctx.channel.set_permissions(ctx.guild.default_role, read_messages=False)
-			if lt > 0:
-				await ctx.send(f"Locked {ctx.message.channel.mention} for `{lt}`s")
-				await asyncio.sleep(lt)
-				await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-				await ctx.channel.set_permissions(ctx.guild.default_role, read_messages=False)
-				await ctx.send("Lockdown Ended")
-			else:
-				await ctx.send(f"Locked {ctx.message.channel.mention} Indefinitely")
-		else:
-			await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-			if lt > 0:
-				await ctx.send(f"Locked {ctx.message.channel.mention} for `{lt}`s")
-				await asyncio.sleep(lt)
-				await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-				await ctx.send("Lockdown Ended")
-			else:
-				await ctx.send(f"Locked {ctx.message.channel.mention} Indefinitely")
-		
-@client.command(aliases = ['UNLOCK' , 'Unlock'])
-async def unlock(ctx):
-	if ctx.message.author.guild_permissions.manage_channels:
-		await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
-		if not ctx.guild.default_role.read_messages: 
-			await ctx.channel.set_permissions(ctx.guild.default_role, read_messages=False)
-		else:
-			await ctx.channel.set_permissions(ctx.guild.default_role, read_messages=True)
-		await ctx.send(f"Unlocked {ctx.message.channel.mention}")
+
 	
 @client.event
 async def on_message(message):
