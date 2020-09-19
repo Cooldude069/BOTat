6950714b87dd7a8b7fb68ddf68fa9a460b3ba8aa
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-19 19:57:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,18 +30,6 @@ async def on_ready():
 	change_status.start()
 	print("Bot is ready.")
 	
-@client.command()
-async def vc(ctx , code = None , server = None):
-	if ctx.author.guild.id == 723435494578323476:
-		category = discord.utils.get(ctx.author.guild.categories , id = 727100421641994322)
-	else:
-		category = discord.utils.get(ctx.author.guild.categories , id = ctx.message.channel.category_id)
-	await ctx.author.guild.create_voice_channel(name = f"l🎯l-{code} -> {server}" , category = category)
-	await ctx.send("Voice channel created successfully! , It will be deleted after 30 seconds")
-	await asyncio.sleep(30)
-	vch = discord.utils.get(ctx.author.guild.voice_channels , name = f"l🎯l-{code} -> {server}" )
-	await vch.delete()
-	
 	
 
 @client.command(aliases = ['Warn' , "WARN"])
@@ -167,6 +155,12 @@ async def info_mirahq(ctx):
 	
 @client.command(aliases = ['Instant_invite' , 'INSTANT_INVITE' , 'II' , 'ii', 'Ii'])
 async def instant_invite(ctx, code = None , server = None):
+	if ctx.author.guild.id == 723435494578323476:
+		category = discord.utils.get(ctx.author.guild.categories , id = 727100421641994322)
+	else:
+		category = discord.utils.get(ctx.author.guild.categories , id = ctx.message.channel.category_id)
+	await ctx.author.guild.create_voice_channel(name = f"l🎯l-{code} -> {server}" , category = category)
+	await ctx.send("Voice channel created successfully! , It will be deleted after 30 minutes")
 	role = discord.utils.get(ctx.author.guild.roles , name = 'Among Us')
 	for member in role.members:
 		await member.create_dm()
@@ -177,6 +171,10 @@ async def instant_invite(ctx, code = None , server = None):
 		embed.set_thumbnail(url = 'https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO')
 		await member.dm_channel.send(embed = embed)
 	await ctx.send("Instant invite sent successfully")
+	await asyncio.sleep(1770)
+	vch = discord.utils.get(ctx.author.guild.voice_channels , name = f"l🎯l-{code} -> {server}" )
+	await vch.delete()
+	await ctx.send(f"{ctx.author.mention}, your request timed out. Voice channel successfully deleted")
 	
 	
 	
