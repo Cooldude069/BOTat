cd08cc7a7ccb1216f5ae1ab42e7f1c2c441a5ffd
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-20 15:40:28 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def testing(ctx, url):
 	server = ctx.message.server
 	await client.say ('Music now playing...')
 	voice_client = client.voice_client_in(server)
-	player = await voice_client.create_ytdl_player(url, ytdl_options={'default_search': 'auto'} after=lambda: check_queue(server.id))
+	player = await voice_client.create_ytdl_player(url, ytdl_options={'default_search': 'auto'} ,  after=lambda: check_queue(server.id))
 	players[server.id] = player
 	player.start()
 	
