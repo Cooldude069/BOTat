cd01489feef3514a422d4f37670207393b22864e
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-09-25 13:28:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,6 +190,17 @@ async def info_mirahq(ctx):
 		mira = discord.Embed(title = 'Mira HQ' , color = discord.Color.orange())
 		mira.set_image(url = 'https://vignette.wikia.nocookie.net/among-us-wiki/images/0/0a/Mirahq.png/revision/latest?cb=20200907132939')
 		await ctx.send(embed = mira)
+
+@client.command()
+async def breaking_news(ctx , * , news = ""):
+	spaces = "** **"
+	spade = 0
+	nmsg = await ctx.send(news)
+	while spade < 10:
+		for i in range(15):
+			times = spaces * i
+			await nmsg.edit(content = f"{times}{news}")
+			await asyncio.sleep(0.5)
 	
 @client.command(aliases = ['Instant_invite' , 'INSTANT_INVITE' , 'II' , 'ii', 'Ii'])
 async def instant_invite(ctx, code = None , server = None):
