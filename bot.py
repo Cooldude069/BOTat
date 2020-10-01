2e0ece9d32351dbd6756003ef2cd57a9608c3773
Cooldude069 <70008729+Cooldude069@users.noreply.github.com>
2020-10-01 12:52:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,8 +35,6 @@ async def on_ready():
 	print("Bot is ready.")
 
 
-
-	
 @tasks.loop(minutes = 1)	
 async def dandt():
 	server = discord.utils.get(client.guilds , id = 723435494578323476)
@@ -907,40 +905,6 @@ async def slowend(ctx):
 	if ctx.message.author.guild_permissions.manage_channels:
 		await ctx.message.channel.edit(slowmode_delay = 0)
 		await ctx.send("Slowmode removed")
-
-@register_call("whoIs")
-def who_is(query, session_id="general"):
-	try:
-		return wikipedia.summary(query)
-	except Exception:
-		for new_query in wikipedia.search(query):
-			try:
-				return wikipedia.summary(new_query)
-			except Exception:
-				pass
-	return "I don't know about "+query
-template_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"chatbottemplate.template")
-chat=Chat(template_file_path)
-
-@client.command(pass_context = True , aliases = ["Wiki" , "WIKI" , "Quesry" , "QUERY" , "query"])
-async def wiki(ctx, *,message):
-	result = chat.respond(message)
-	if(len(result)<=2048):
-		embed=discord. Embed(title="ChatBot AI", description = result, color = (0xF48D1))
-		await ctx.send(embed=embed)
-	else:
-		embedList = []
-		n=2048
-		embedList = [result[i:i+n] for i in range(0, len(result), n)]
-		for num, item in enumerate(embedList, start = 1):
-			if(num == 1):
-				embed = discord. Embed(title="ChatBot AI", description = item, color = (0xF48D1))
-				embed.set_footer(text="Page{}".format(num))
-				await ctx.send(embed = embed)
-			else:
-				embed = discord.Embed(description = item, color = (0xF48D1))
-				embed.set_footer(text = "Page {}".format(num))
-				await ctx.send(embed = embed)
 
 	
 @client.event
